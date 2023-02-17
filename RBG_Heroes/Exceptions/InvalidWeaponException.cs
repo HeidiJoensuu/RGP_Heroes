494ab6193b87dc92cbb9ae5ada29267566e91831
Heidi Joensuu <heidi.joensuu@fi.experis.com>
2023-02-17 16:03:14 +0200

    readme

---
+++ RBG_Heroes/Exceptions/InvalidWeaponException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RBG_Heroes.Exceptions
+{
+    /// <summary>
+    /// Invalid Weapon - custom exception
+    /// </summary>
+    public class InvalidWeaponException: Exception
+    {
+    }
+}
